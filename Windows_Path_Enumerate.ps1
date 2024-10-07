6b95c841944e777d137db963a0b3e2b5629b6e2a
Severn <75281574+severn-pccm@users.noreply.github.com>
2024-10-07 09:42:55 +0100

    Update Windows_Path_Enumerate.ps1

--- Windows_Path_Enumerate.ps1
+++ Windows_Path_Enumerate.ps1
@@ -565,3 +565,10 @@ if ($RestoreBackup){
 if ($DeleteLogFile){
     Remove-Item $LogName -Force -ErrorAction "SilentlyContinue"
 }
+
+# NinjaOne Specific Variables:
+# Store the output in a variable
+$UnquotedServicePathResult = $ScriptExecutionResult
+ 
+#Outputs to a NinjaOne custom field.
+Ninja-Property-Set unquotedServicePathReport $UnquotedServicePathResult
